983f3da9507ac812329c2637fe4c42494ae8ff5e
Ao Li <leeleo3x@gmail.com>
2020-12-21 13:03:31 -0500

    update.

--- simplifier.py
+++ simplifier.py
@@ -2,9 +2,9 @@ from pybatfish.client.commands import *
 from pybatfish.question.question import load_questions
 from pybatfish.question import bfq
 from pybatfish.datamodel.flow import HeaderConstraints, PathConstraints
-from typing import List
+from typing import List, Set
 
-bf_session.host = 'localhost'
+
 class ConfigurationSimplifier(object):
     def __init__(self, snapshot):
         bf_init_snapshot(snapshot, "origin", overwrite=True)
@@ -14,14 +14,15 @@ class ConfigurationSimplifier(object):
         for _, result in results.iterrows():
             self.nodes.append(result.Node)
 
-    def unreachable_switches(self, switches: List[str]) -> List[str]:
+    def unreachable_switches(self, switches: List[str]) -> Set[str]:
         pending_nodes = set(self.nodes)
         pending_nodes.difference_update(switches)
         for switch in switches:
             reserved = set()
             for pending_node in pending_nodes:
                 results = bfq.reachability(
-                    pathConstraints=PathConstraints(startLocation=switch, endLocation=pending_node)).answer().frame()
+                    pathConstraints=PathConstraints(startLocation=switch, endLocation=pending_node)).answer()
+                results = results.frame()
                 if results.size == 0:
                     continue
                 reserved.add(pending_node)
@@ -32,3 +33,6 @@ class ConfigurationSimplifier(object):
             pending_nodes.difference_update(reserved)
         return pending_nodes
 
+
+s = ConfigurationSimplifier("/home/leo/repos/verifier/configs/test")
+print(s.unreachable_switches(["as2dept1"]))
